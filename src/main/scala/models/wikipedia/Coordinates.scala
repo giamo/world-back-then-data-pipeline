4c3ff19a8b4981460495dfa87f6edec61b4edaf9
Gianluca Amori <gianluca.amori@gmail.com>
2020-04-24 13:17:15 +0200

    Implement parser of coordinates template

--- src/main/scala/models/wikipedia/Coordinates.scala
+++ src/main/scala/models/wikipedia/Coordinates.scala
@@ -1,3 +1,47 @@
 package models.wikipedia
 
-final case class Coordinates(lat: Double, long: Double)
+import scala.util.Try
+
+final case class Coordinates(latitude: Double, longitude: Double, region: Option[String] = None)
+
+object Coordinates {
+  private val degreesRegex = "\\{\\{coord\\|([0-9]+)(?:\\|([0-9]+))?(?:\\|([0-9]+))?\\|(n|s)\\|([0-9]+)(?:\\|([0-9]+))?(?:\\|([0-9]+))?\\|(e|w).*\\}\\}".r
+  private val decimalRegex = "\\{\\{coord\\|([\\-]?[0-9\\.]+)(?:\\|([n|s]))?\\|([\\-]?[0-9\\.]+)(?:\\|([e|w]))?\\|.*\\}\\}".r
+
+  def fromTemplate(text: String): Option[Coordinates] = text.toLowerCase match {
+    case degreesRegex(degreeLat, minuteLat, secondLat, directionLat, degreeLong, minuteLong, secondLong, directionLong) =>
+      for {
+        lat <- convert(degreeLat, Option(minuteLat), Option(secondLat), directionLat)
+        long <- convert(degreeLong, Option(minuteLong), Option(secondLong), directionLong)
+      } yield Coordinates(lat, long)
+    case decimalRegex(latitude, directionLat, longitude, directionLong)                                                 =>
+      for {
+        latVal <- toDouble(latitude).map(truncate(_))
+        longVal <- toDouble(longitude).map(truncate(_))
+        lat = evaluateDirection(latVal, directionLat)
+        long = evaluateDirection(longVal, directionLong)
+      } yield Coordinates(lat, long)
+    case _                                                                                                              => None
+  }
+
+  private def convert(degrees: String, minutes: Option[String], seconds: Option[String], direction: String): Option[Double] = {
+    for {
+      d <- toDouble(degrees)
+      m <- toDouble(minutes)
+      s <- toDouble(seconds)
+      dd = truncate(d + m / 60 + s / (60 * 60))
+    } yield evaluateDirection(dd, direction)
+  }
+
+  private def evaluateDirection(value: Double, direction: String): Double =
+    if ("s" == direction || "w" == direction) -1 * value
+    else value
+
+  private def toDouble(s: String): Option[Double] = Try(s.toDouble).toOption
+
+  private def toDouble(s: Option[String]): Option[Double] =
+    s.fold(Option(0d))(toDouble)
+
+  private def truncate(d: Double, decimals: Int = 3) =
+    BigDecimal(d).setScale(decimals, BigDecimal.RoundingMode.HALF_UP).toDouble
+}
---
+++ src/test/scala/models/wikipedia/CoordinatesTest.scala
@@ -0,0 +1,40 @@
+package models.wikipedia
+
+import cats.implicits._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+final class CoordinatesTest extends AnyFlatSpec with Matchers {
+
+  import Coordinates._
+
+  "Parsing a coordinates string" should "extract and compute the right lat and long" in {
+    fromTemplate("{{coord|40|25|4|N|85|38|56|W|region:US-IN|display=inline,title}}") should ===(
+      Coordinates(40.418, -85.649).some
+    )
+    fromTemplate("{{coord|8|50|18|S|13|14|4|E|region:AO|display=inline}}") should ===(
+      Coordinates(-8.838, 13.234).some
+    )
+    fromTemplate("{{coord|11|08|S|27|06|E|type:adm1st_region:CD|display=inline,title}}") should ===(
+      Coordinates(-11.133, 27.1).some
+    )
+    fromTemplate("{{coord|20|N|40|E|type:adm1st_region:CD|display=inline,title}}") should ===(
+      Coordinates(20, 40).some
+    )
+  }
+
+  it should "extract decimal lat and long when explicit" in {
+    fromTemplate("{{coord|27.7177|85.3240|type:city_region:NP|display=it}}") should ===(
+      Coordinates(27.718, 85.324).some
+    )
+    fromTemplate("{{coord|30|-60|type:city_region:NP|display=it}}") should ===(
+      Coordinates(30, -60).some
+    )
+    fromTemplate("{{coord|46.995|N|120.549|W|region:US-WA|display=inline,title}}") should ===(
+      Coordinates(46.995, -120.549).some
+    )
+    fromTemplate("{{coord|55.6666|S|22.3333|E|region:US-WA|display=inline,title}}") should ===(
+      Coordinates(-55.667, 22.333).some
+    )
+  }
+}

b31dfa54a3ca8915e14eeff0915b332e13fba286
Gianluca Amori <gianluca.amori@gmail.com>
2020-05-09 14:43:38 +0200

    Add synopsis to parsed archaeological cultures

--- src/main/scala/com/github/giamo/wikihistory/models/wikipedia/infoboxes/ArchaeologicalCulture.scala
+++ src/main/scala/com/github/giamo/wikihistory/models/wikipedia/infoboxes/ArchaeologicalCulture.scala
@@ -1,18 +1,20 @@
 package com.github.giamo.wikihistory.models.wikipedia.infoboxes
 
-import com.github.giamo.wikihistory.models.{Date, DateRange}
+import com.github.giamo.wikihistory.models.{Date, DateRange, SpecificDate}
 import com.github.giamo.wikihistory.models.wikipedia.WikiPage
 
 final case class ArchaeologicalCulture(
   pageId: Long,
   pageTitle: String,
   name: String,
+  synopsis: String = "",
   region: Option[String],
   dates: Option[String]
 ) {
   def isIncludedinRange(from: Int, to: Int): Boolean = {
     dates.flatMap(d => Date.fromString(d).toOption) match {
       case Some(DateRange(f, t)) => f.toYear <= from && t.toYear >= to
+      case Some(d: SpecificDate) => d.toYear <= from && d.toYear >= to
       case _ => false
     }
   }
@@ -37,6 +39,7 @@ object ArchaeologicalCulture extends Infobox[ArchaeologicalCulture] {
         pageId = page.id,
         pageTitle = page.title,
         name = name,
+        synopsis = WikiPage.getHtmlSynopsis(page.text),
         region = region,
         dates = dates
       )

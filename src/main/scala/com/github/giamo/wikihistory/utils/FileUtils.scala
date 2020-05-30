81c40df892799030b7ff176cc0f81d60cb949f9d
Gianluca Amori <gianluca.amori@gmail.com>
2020-05-30 19:50:44 +0200

    Add utility to list files in directory

---
+++ src/main/scala/com/github/giamo/wikihistory/utils/FileUtils.scala
@@ -0,0 +1,13 @@
+package com.github.giamo.wikihistory.utils
+
+import java.io.File
+
+object FileUtils {
+
+  def getListOfFiles(dir: String):List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory) d.listFiles.filter(_.isFile).toList
+    else List[File]()
+  }
+
+}
